32d01092935d48e338af8b3839774ac8f348a6de
Jonathan Vagnier <adm.jvagnier@gmail.com>
2022-01-05 16:10:48 +0100

    Correction des cubes

--- jquery/exo/main.js
+++ jquery/exo/main.js
@@ -1,29 +1,27 @@
-function AnimationRouge (){
-  $("#div3").animate({left: '500px'}, 3000)
-  $("#div3").animate({top: '500px'}, 3000)
-  $("#div3").animate({left: '50px'}, 3000)
-  $("#div3").animate({top: '50px'}, 3000)
-}
-
-function AnimationBleu (){
+function Animation(){
   $("#div4").animate({left: '50px'}, 3000)
   $("#div4").animate({top: '50px'}, 3000)
   $("#div4").animate({left: '500px'}, 3000)
   $("#div4").animate({top: '500px'}, 3000)
+
+  $("#div3").animate({left: '500px'}, 3000)
+  $("#div3").animate({top: '500px'}, 3000)
+  $("#div3").animate({left: '50px'}, 3000)
+  $("#div3").animate({top: '50px'}, 3000)
+
+  Animation()
 }
 
-$("body").on('animationRougeEvent', AnimationRouge)
-$("body").on('animationBleuEvent', AnimationBleu)
+$(document).on('animationEvent', Animation)
 
 $(document).ready(function(){
 
   $("#btn1").click(function() {
-    $(this).trigger('animationRougeEvent')
-    $(this).trigger('animationBleuEvent')
+    Animation()
   })
 
   $("#btn2").click(function(){
-    $("#div3").stop(true,true)
-    $("#div4").stop(true,true)
+    $("#div3").stop(true,false)
+    $("#div4").stop(true,false)
   })
 })
