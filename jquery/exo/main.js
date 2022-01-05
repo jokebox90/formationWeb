e0db789fc740781bd08226a1df6b5ed15cab6822
Jonathan Vagnier <adm.jvagnier@gmail.com>
2022-01-05 16:56:41 +0100

    Change la fleche de sens

--- jquery/exo/main.js
+++ jquery/exo/main.js
@@ -1,11 +1,12 @@
 function enregistreClick(question, reponse) {
   $(question).click(function() {
+    $(question).children().toggleClass('collapse');
     $(reponse).fadeToggle();
   })
 }
 
 $(document).ready(function(){
-  enregistreClick('#q1', '#a1')
-  enregistreClick('#q2', '#a2')
-  enregistreClick('#q3', '#a3')
+  enregistreClick('#q1', '#a1');
+  enregistreClick('#q2', '#a2');
+  enregistreClick('#q3', '#a3');
 })
